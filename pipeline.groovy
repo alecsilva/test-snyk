0a6b4f23c41af4bf3cb41825ce2e0bda5111bfc7
Alexander Silva <alexfrandi@gmail.com>
2024-12-02 10:39:58 -0500

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,17 @@
+pipeline {
+  agent any
+
+  stage('Snyk Open Source Scan - SCA') {
+    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+        snykSecurity additionalArguments: '-debug', failOnIssues: false, failOnError: false, monitorProjectOnBuild: true, 
+                                                                projectName: '${JOB_NAME}', severity: 'high', snykInstallation: 'snyk@latest', 
+                                                                snykTokenId: 'snyk-api-token'
+          }
+      }
+        
+    stage('Snyk Code Scan - SAST') {
+        snykSecurity additionalArguments: '--code -debug', failOnIssues: false, failOnError: false, monitorProjectOnBuild: true,
+                                                        projectName: '${JOB_NAME}', snykInstallation: 'snyk@latest', 
+                                                        snykTokenId: 'snyk-api-token'
+    }
+}
